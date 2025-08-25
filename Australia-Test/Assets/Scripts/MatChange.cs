a13f37766521f36019631362d018411c052f157a
Mac0r0 <max.candiloro@gmail.com>
2025-08-26 02:46:39 +1000

    Colour change mechanic test

---
+++ Australia-Test/Assets/Scripts/MatChange.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MatChange : MonoBehaviour
+{
+    [SerializeField] Renderer myObject;
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myObject.material.color = Color.red;
+        }
+    }
+}
