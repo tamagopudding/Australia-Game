362db2d102513fc57c98a7a04a1c9b66b3b4dcbc
tamagopudding <annabellesetiawan2005@gmail.com>
2025-09-08 18:40:35 +1000

    Fixed coral shader + sand texture

--- Australia-Test/Assets/Scripts/SoundController.cs
+++ Australia-Test/Assets/Scripts/SoundController.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 public class SoundControler : MonoBehaviour
 {
     AudioSource source;
+    AudioSource waterSource;
     Collider soundTrigger;
+    Collider underwaterSound;
 
     private void Awake()
     {
         source = GetComponent<AudioSource>();
         soundTrigger = GetComponent<Collider>();
+        underwaterSound = GetComponent<Collider>();
     }
 
     void OnTriggerEnter(Collider other)
     {
         source.Play();
+        waterSource.Play(); 
     }
 }
